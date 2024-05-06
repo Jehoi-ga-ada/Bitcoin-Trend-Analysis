dc7e1deee68bb20150df5d6f047d71d516240b9c
Jehoiada Wong <sayajehoiada@gmail.com>
2024-05-06 17:01:32 +0700

    removed models module for better management

--- models.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import keras as k
-from keras.layers import *
-from keras.models import Sequential
-
-baseline = Sequential([
-    LSTM(4),
-    Dense(1, activation='sigmoid')
-])
